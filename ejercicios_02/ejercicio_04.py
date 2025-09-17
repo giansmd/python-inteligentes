2f440d00d92117dd451fb8e436051762aa8d70c2
Gian Franco Samana <gian.samana.1801@gmail.com>
2025-09-17 14:18:20 -0500

    feat(sem2): ejercicio 4

---
+++ ejercicios_02/ejercicio_04.py
@@ -0,0 +1,56 @@
+from utils.utils import ingresar_ne, ingresar_nn, ingresar_str
+
+
+def ingresar_nombre_estudiante(n: int):
+    return ingresar_str(f"el nombre del estudiante {n}")
+
+
+def ingresar_nota_estudiante(n: int):
+    return ingresar_ne(f"una nota del estudiante {n}")
+
+
+def registrar_dict_alumnos():
+    dict_alumnos = {}
+    cantidad_alumnos = ingresar_nn("la cantidad de alumnos")
+    for i in range(cantidad_alumnos):
+        # PASO. registrar nombre
+        while True:
+            nombre_estudiante = ingresar_nombre_estudiante(i+1)
+            if dict_alumnos.get(nombre_estudiante) is None:
+                break
+            print("Error: el nombre ya existe en el diccionario. Esbriba otro.")
+
+        # PASO. si el nombre no existe, registrar la lista de notas
+        lista_notas_estudiante = []
+        while True:
+            nota_estudiante = ingresar_nota_estudiante(i+1)
+            if nota_estudiante >= 0 & nota_estudiante <= 20:
+                lista_notas_estudiante.append(nota_estudiante)
+            elif len(lista_notas_estudiante) == 0:
+                print("Escbriba al menos una nota.") 
+            else:
+                break
+        dict_alumnos[nombre_estudiante] = lista_notas_estudiante
+        
+    return dict_alumnos
+
+
+dict_alumnos = registrar_dict_alumnos()
+
+
+def get_alumnos_con_nota_promedio(dict_alumnos):
+    dict_alumnos_con_nota_promedio = {}
+
+    for alumno in dict_alumnos.items():
+        nombre = alumno[0]
+        notas = alumno[1]
+        suma = 0
+        for nota in notas:
+            suma += nota
+        promedio = suma / len(notas)  # tiene que ingresar al menos una nota
+        dict_alumnos_con_nota_promedio[nombre] = round(promedio, 2)
+
+    return dict_alumnos_con_nota_promedio
+
+
+print(get_alumnos_con_nota_promedio(dict_alumnos))
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,24 @@
+def ingresar_ne(p_label: str):
+    while True:
+        try:
+            number = int(input(f"Ingrese {p_label}: "))
+            return number
+        except ValueError:
+            print("Error: Por favor ingrese un número entero válido.")
+
+
 def ingresar_n(p_label: str):
+    return ingresar_nn(p_label)
+
+
+def ingresar_nn(p_label: str):
     while True:
         try:
             number = int(input(f"Ingrese {p_label}: "))
             if number > 0:
                 break
         except ValueError:
-            print("Error: Por favor ingrese un número entero válido.")
+            print("Error: Por favor ingrese un número natural válido.")
     return number
 
 
